8fb086bbe4046f21e9f99441e879aa7f108c1e87
GuiMassucatto <guimassucatto2@gmail.com>
2024-11-01 11:44:36 -0300

    primeiro commit

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const clienteController = require('./controller/controleCliente');
+const telefoneController = require('./controller/controleTelefone');
+const carroController = require('./controller/controleCarros');
+
+//Routes Cliente
+router.get('/clientes', clienteController.listarClientes);
+router.post('/clientes', clienteController.criarCliente);
+router.delete('/clientes/:id', clienteController.deletarCliente);
+router.put('/clientes/:id', clienteController.atualizarCliente);
+
+//Routes Telefone
+router.post('/telefones', telefoneController.criarTelefone);
+router.put('/telefones/:id', telefoneController.atualizarTelefone);
+router.get('/telefones', telefoneController.listarTelefones);
+router.delete('/telefones/:id', telefoneController.deletarTelefone);
+
+//Routes Carro
+router.post('/carros', carroController.criarCarros);
+router.get('/carros', carroController.listarCarros);
+router.put('/carros/:id', carroController.atualizarCarros);
+router.delete('/carros/:id', carroController.deletarCarro);
+
+module.exports = router;
